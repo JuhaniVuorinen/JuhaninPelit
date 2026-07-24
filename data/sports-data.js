80a475c60dee0649118a54d33192aa67fc519107
JuhaniVuorinen <101091205+JuhaniVuorinen@users.noreply.github.com>
2026-07-24 14:20:47 +0300

    Add sports genre data with game titles and covers

---
+++ data/sports-data.js
@@ -0,0 +1,32 @@
+const genreData = [
+  {
+    category: "URHEILU & MOOTTORIURHEILU",
+    covers: [
+      "covers/ea-fc.jpg",
+      "covers/nba2k.jpg",
+      "covers/f1.jpg"
+    ],
+    games: [
+      "EA Sports FC 27",
+      "NBA 2K27",
+      "NHL 26",
+      "F1 26",
+      "Gran Turismo 7",
+      "WWE 2K26",
+      "Rocket League",
+      "Forza Horizon 5",
+      "Assetto Corsa EVO",
+      "Madden NFL 27",
+      "TopSpin 2K25",
+      "EA Sports WRC",
+      "Riders Republic",
+      "Need for Speed Unbound",
+      "UFC 5",
+      "PGA Tour 2K25",
+      "Wreckfest 2",
+      "The Crew Motorfest",
+      "Hot Wheels Unleashed 2",
+      "Lonely Mountains: Downhill"
+    ]
+  }
+];
